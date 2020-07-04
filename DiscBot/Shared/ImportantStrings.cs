6c0045a853764884ee64d64f779c7fcebfb1886c
Jandere <iutzhanov@bk.ru>
2020-07-04 22:06:45 +0600

    [delete] BotTokenKey

--- DiscBot/Shared/ImportantStrings.cs
+++ DiscBot/Shared/ImportantStrings.cs
@@ -3,7 +3,7 @@
     public static class ImportantStrings
     {
         public const string ChannelToSay = "бот-говорит";
-        public const string BotToken = "NzI2Mzg2OTc2NjQ2ODg5NTAz.Xv9Hpw.PBYoiNEsHWxtOfoVzJ6O9m0BYCw";
+        public const string BotToken = "";
         public const string ChannelForGame = "Мафия";
     }
 }
